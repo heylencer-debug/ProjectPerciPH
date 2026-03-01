1ee2deecf79c93df0d87c5710a6332772919e4bb
heylencer-debug <heylencer@gmail.com>
2026-03-02 04:47:29 +0800

    fix: replace service role key with anon key

--- data/sb.js
+++ data/sb.js
@@ -2,5 +2,5 @@
 // Key split to avoid GitHub secret scanning on public repo
 window.SUPABASE_CONFIG = {
   url: 'https://fhfqjcvwcxizbioftvdw.supabase.co',
-  key: ['sb_secret_Urw2XKj4d9', 'QUsvcEnQrKBA_TzA_KEnH'].join('')
+  key: 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImZoZnFqY3Z3Y3hpemJpb2Z0dmR3Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzIzNTcxMzgsImV4cCI6MjA4NzkzMzEzOH0.g8K40DjhvxE7u4JdHICqKc1dMxS4eZdMhfA11M8ZMBc'
 };
