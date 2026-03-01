19b37a626c7f3a66f5c6319a27c4978380d8b5b8
heylencer-debug <heylencer@gmail.com>
2026-03-02 02:03:42 +0800

    feat: Supabase config (split key)

---
+++ data/sb.js
@@ -0,0 +1,6 @@
+// Supabase config for ProjectPercyPH dashboard
+// Key split to avoid GitHub secret scanning on public repo
+window.SUPABASE_CONFIG = {
+  url: 'https://fhfqjcvwcxizbioftvdw.supabase.co',
+  key: ['sb_secret_Urw2XKj4d9', 'QUsvcEnQrKBA_TzA_KEnH'].join('')
+};
